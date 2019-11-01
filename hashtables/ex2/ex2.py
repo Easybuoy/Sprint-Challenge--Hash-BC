429a7b48f17ff7b3007d3b490c02ea8a14db7cd3
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2019-11-01 11:21:22 +0100

    asolve ext2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -21,20 +21,35 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     for i in range(len(tickets)):
-        # print(tickets[i])
         hash_table_insert(hashtable, tickets[i].source, tickets[i].destination)
-        print(hash_table_retrieve(hashtable, tickets[i].source), tickets[i-1].source)
-        # if (hash_table_retrieve(hashtable, tickets[i].source) ):
-        route[i] = hash_table_retrieve(hashtable, tickets[i].source)
 
-        # print(hash(tickets[i].source, tickets[i].destination))
+    route[0] = hash_table_retrieve(hashtable, 'NONE')
+
+    for i in range(1, len(route)):
+        route[i] = hash_table_retrieve(hashtable, route[i-1])
     return route
 
 
-ticket_1 = Ticket("NONE", "PDX")
-ticket_2 = Ticket("PDX", "DCA")
-ticket_3 = Ticket("DCA", "NONE")
+# ticket_1 = Ticket("NONE", "PDX")
+# ticket_2 = Ticket("PDX", "DCA")
+# ticket_3 = Ticket("DCA", "NONE")
 
-tickets = [ticket_1, ticket_2, ticket_3]
+# tickets = [ticket_1, ticket_2, ticket_3]
 
-print(reconstruct_trip(tickets, 3))
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "SAP")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("SAP", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
+
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+           ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+
+# expected = ["LAX", "SFO", "BHM", "FLG", "XNA", "SAP",
+#             "SLC", "PIT", "ORD", "NONE"]
+print(reconstruct_trip(tickets, 10))
